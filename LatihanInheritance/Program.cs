431658f43c483180af06bde2b118b68af2a26dbb
Lincchi <caroline.lincchi@gmail.com>
2020-04-09 12:41:07 +0700

    Constructor Inheritance

---
+++ LatihanInheritance/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace LatihanInheritance
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person person = new Person("Mike", 20);
+            person.GetNameAndAge();
+
+            Teacher teacher = new Teacher("Thomas", 35, "190302", "Math");
+            teacher.GetNameAndAge();
+
+            Student student = new Student("Elijah", 19, "19.11.2697", "elijah@amikom.ac.id");
+            student.GetNameAndAge();
+
+            Console.ReadKey();
+        }
+    }
+}
